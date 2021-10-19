32364342a5a194f2bead2341e05cc672a074a29f
João Sereia <joao.lo.sereia@gmail.com>
2021-10-19 14:22:23 -0300

    Alteração do header padding ios

--- src/styles/metrics.js
+++ src/styles/metrics.js
@@ -3,7 +3,7 @@ import { Platform } from "react-native"
 export default {
     padding: 16,
     ...Platform.select({
-        ios: { headerHeight: 64, headerPadding: 24 },
+        ios: { headerHeight: 64, headerPadding: 28 },
         android: { headerHeight: 80, headerPadding: 0 }
     }),
 
