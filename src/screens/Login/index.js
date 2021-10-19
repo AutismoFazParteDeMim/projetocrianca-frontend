ba5b3a0de9be6c5914956de0f53fd33c4f2c23c5
BDu4rTe <brianschierduarte@outlook.com>
2021-10-18 21:05:44 -0300

    Criação da tela de login

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,15 +1,23 @@
 import React from "react"
-import { Text, SafeAreaView, View } from "react-native"
+import { SafeAreaView, View, Image } from "react-native"
 
 import styles from "./styles"
 import Header from "../../components/Header"
+import InputText from "../../components/Input/InputText"
+import InputPass from "../../components/Input/InputPass"
+import Button from "../../components/Button"
 
 function Login({ navigation }) {
     return (
         <SafeAreaView style={styles.globalContainer}>
             <Header title="Login" navigation={navigation} />
             
-            <View>
+            <View style={[styles.container]}>
+
+                <InputText type="email_login" icon="mail-outline" placeholder="Insira seu e-mail" />
+                <InputPass type="password" icon="lock-closed-outline" placeholder="Insira sua senha" />
+                <Button icon="enter-outline" iconPosition="left" title="Entrar" />
+                <Image source={require('../../../assets/animais.png')} style={styles.animals} />
                 
             </View>
         </SafeAreaView>
--- src/screens/Login/styles.js
+++ src/screens/Login/styles.js
@@ -3,6 +3,19 @@ import { colors, global, metrics } from "../../styles"
 
 const styles = StyleSheet.create({
     ...global,
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+
+    animals:{
+        flex: 1,
+        width: 266,
+        heigth: 256, 
+        marginBottom: 16,
+        marginTop: 464,  
+    },
 })
 
 export default styles
