1a4bf30dddb0e8fdd8eb33f7415465564e4d7d9a
Guilherme <guilherme09112001@gmail.com>
2021-10-31 05:15:28 -0300

    Foi adicionado componentes na page home

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,10 +1,11 @@
 import React from "react"
 import { Text, View, Image } from "react-native"
-
+import {colors, Icon} from "../../styles"
 
 import styles from "./styles"
 import InputText from "../../components/Input/InputText"
 import InputPass from "../../components/Input/InputPass"
+import InputSearch from "./components/InputSearch"
 import Button from "../../components/Button"
 import CustomModal from "../../components/Modal"
 
@@ -12,11 +13,16 @@ function Home({ navigation }) {
     return (
         <View style={[styles.globalContainer]}>
             {/* Aqui vai o HeaderHome com a imagem e o search */}
-
-            <View>
-                <Text style={[styles.headerHomeText]}>Bem vindo(a)!!!</Text>
-                <Text style={[styles.headerHomeText]}>Username:</Text>
+            <View style={styles.headerHome}>
+                <Icon name={"person-outline"} style={styles.iconPerson} />
+                <InputSearch placeholder={'Pesquise aqui.'} icon={'ios-search'} />
             </View>
+
+            <View style={styles.welcome}>
+                <Text style={[styles.titleHome]}>Bem vindo(a)!!!</Text>
+                <Text style={[styles.titleHome]}>Username:</Text>
+            </View>
+            
             <View style={[styles.container]}>
                 <Text> Os botões vão logo a baixo</Text>
             </View>
--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -3,16 +3,69 @@ import { colors, global, metrics, fonts } from "../../styles"
 
 const styles = StyleSheet.create({
     ...global,
-    container: {
-        flex: 1,
+    container:{
+        flex:1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: "100%",
+        height: metrics.button.height,
+        marginBottom: metrics.padding
+    },
+    headerHome: {
+        flex: 0,
+        flexDirection: 'row',
+    },
+    iconPerson: {
+        width: "30%",
+        fontSize: 50,
+        height: metrics.button.height,
+        marginBottom: metrics.padding,
+    },
+    containerInputSearch:{
+        width: "70%",
+        height: metrics.button.height,
+        marginBottom: metrics.padding,
+        alignItems: 'flex-end',
+    },
+    wrapper: {
+        width: "100%",
+        height: "100%",
+        paddingRight: metrics.padding,
+        flexDirection: "row",
+        borderRadius: metrics.button.borderRadius,
+        borderWidth: 5,
+        borderColor: colors.primary,
+        backgroundColor: colors.inputBackground
+    },
+    iconWrapper: {
+        height: "100%",
+        paddingLeft: metrics.padding,
+        paddingRight: metrics.padding,
         alignItems: "center",
         justifyContent: "center",
+        borderRadius: metrics.button.borderRadius,
+        //marginRight: metrics.padding,
+        //backgroundColor: colors.inputBackground_shadow,
+        color: colors.text_alt
     },
-    headerHomeText: {
-        fontSize: 20,
-        fontWeight:'bold',
+    icon: {
+        fontSize: metrics.button.iconSize,
         color: colors.text
-    }
+    },
+    input: {
+        flex: 1,
+        fontFamily: fonts.text.font,
+        fontSize: fonts.text.size,
+        paddingTop: fonts.text.paddingTop
+    },
+
+    welcome:{
+        marginTop: 26
+    },
+    titleHome:{
+        fontSize: fonts.subTitle.size,
+        fontWeight: 'bold',
+    },
 })
 
 export default styles
