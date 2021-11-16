d9365b8a9a03630f085ce4ab947e60a11e1ff46f
Ewerton <ewerton.viniciusturco@gmail.com>
2021-11-16 18:03:35 -0300

    Sliders de Expressões adicionado

---
+++ src/components/Slides/Expressoes/index.js
@@ -0,0 +1,47 @@
+export const SlidesExpressoes = [
+    {
+        key: '1',
+        text: 'Lorem ipsum urna potenti etiam praesentr fringilla aliquam, vestibulum curabiturkufy imperdiet turpis nisi semper, rutrum metus dipisci ng enim sagittis augue.',
+        image: require('./assets/1Alegre.png')
+    },
+    {
+        key: '2',
+        text: 'Lorem ipsum urna potenti etiam praesentr fringilla aliquam, vestibulum curabiturkufy imperdiet turpis nisi semper, rutrum metus dipisci ng enim sagittis augue.',
+        image: require('./assets/2Confuso.png')
+    },
+    {
+        key: '3',
+        text: 'Lorem ipsum urna potenti etiam praesentr fringilla aliquam, vestibulum curabiturkufy imperdiet turpis nisi semper, rutrum metus dipisci ng enim sagittis augue.',
+        image: require('./assets/3EmDuvida.png')
+    },
+    {
+        key: '4',
+        text: 'Lorem ipsum urna potenti etiam praesentr fringilla aliquam, vestibulum curabiturkufy imperdiet turpis nisi semper, rutrum metus dipisci ng enim sagittis augue.',
+        image: require('./assets/4EstouBem.png')
+    },
+    {
+        key: '5',
+        text: 'Lorem ipsum urna potenti etiam praesentr fringilla aliquam, vestibulum curabiturkufy imperdiet turpis nisi semper, rutrum metus dipisci ng enim sagittis augue.',
+        image: require('./assets/5Feliz.png')
+    },
+    {
+        key: '6',
+        text: 'Lorem ipsum urna potenti etiam praesentr fringilla aliquam, vestibulum curabiturkufy imperdiet turpis nisi semper, rutrum metus dipisci ng enim sagittis augue.',
+        image: require('./assets/6Infeliz.png')
+    },
+    {
+        key: '7',
+        text: 'Lorem ipsum urna potenti etiam praesentr fringilla aliquam, vestibulum curabiturkufy imperdiet turpis nisi semper, rutrum metus dipisci ng enim sagittis augue.',
+        image: require('./assets/7Triste.png')
+    },
+    {
+        key: '8',
+        text: 'Lorem ipsum urna potenti etiam praesentr fringilla aliquam, vestibulum curabiturkufy imperdiet turpis nisi semper, rutrum metus dipisci ng enim sagittis augue.',
+        image: require('./assets/8Raiva.png')
+    },
+    {
+        key: '9',
+        text: 'Lorem ipsum urna potenti etiam praesentr fringilla aliquam, vestibulum curabiturkufy imperdiet turpis nisi semper, rutrum metus dipisci ng enim sagittis augue.',
+        image: require('./assets/9Surpreso.png')
+    },
+]
--- src/routes.js
+++ src/routes.js
@@ -4,13 +4,13 @@ import { NavigationContainer } from "@react-navigation/native"
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
 
 import { colors } from "./styles"
-
 import Welcome from "./screens/Welcome"
 import Login from "./screens/Login"
 import Register from "./screens/Register"
-import Settings from "./screens/Settings";
-import Profile from "./screens/Profile";
+import Settings from "./screens/Settings"
+import Profile from "./screens/Profile"
 import Rotinas from "./screens/Rotinas"
+import Expressoes from "./screens/Expressoes"
 
 
 const Stack = createNativeStackNavigator()
@@ -25,13 +25,14 @@ function Routes() {
                 translucent={false}
                 hidden={false} />
 
-            <Stack.Navigator initialRouteName="Rotinas" screenOptions={{ headerShown: false }}>
+            <Stack.Navigator initialRouteName="Expressoes" screenOptions={{ headerShown: false }}>
                 <Stack.Screen name="Welcome" component={Welcome} />
                 <Stack.Screen name="Login" component={Login} />
                 <Stack.Screen name="Register" component={Register} />
                 <Stack.Screen name="Settings" component={Settings} />
                 <Stack.Screen name="Profile" component={Profile} />
                 <Stack.Screen name="Rotinas" component={Rotinas} />
+                <Stack.Screen name="Expressoes" component={Expressoes} />
             </Stack.Navigator>
         </NavigationContainer>
     )
