99e08bf0f3dc69fac05843cc6b6552570f655613
João Sereia <joao.lo.sereia@gmail.com>
2022-01-16 07:06:40 -0300

    Adição da fonte Poppins

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+import AppLoading from 'expo-app-loading';
+import { useFonts, Poppins_400Regular, Poppins_500Medium, Poppins_700Bold } from '@expo-google-fonts/poppins'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    let [fontsLoaded] = useFonts({
+        "regular": Poppins_400Regular,
+        "medium": Poppins_500Medium,
+        "bold": Poppins_700Bold
+    })
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    if (!fontsLoaded) {
+        return <AppLoading />
+    } else {
+        return (
+            <View style={{ flex: 1 }}>
+                <Text style={{ fontFamily: "bold" }}>Open up App.js to start working on your app!</Text>
+                <Text>Open up App.js to start working on your app!</Text>
+                <StatusBar style="auto" />
+            </View>
+        )
+    }
+}
