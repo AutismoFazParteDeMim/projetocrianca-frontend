5330c3e722595105b8df2f8c16dd16bf914dfa33
João Sereia <joao.lo.sereia@gmail.com>
2022-04-11 18:50:17 -0300

    Alteração nas imagens agora em png

--- src/screens/Activities/Expressions/data.js
+++ src/screens/Activities/Expressions/data.js
@@ -3,54 +3,54 @@ export default [
         key: '1',
         title: "Alegre",
         text: 'Lorem ipsum urna potenti etiam praesentr fringilla aliquam, vestibulum curabiturkufy imperdiet turpis nisi semper, rutrum metus dipisci ng enim sagittis augue.',
-        image: require('./assets/1Alegre.jpg')
+        image: require('./assets/1Alegre.png')
     },
     {
         key: '2',
         title: "Confuso",
         text: 'Lorem ipsum urna potenti etiam praesentr fringilla aliquam, vestibulum curabiturkufy imperdiet turpis nisi semper, rutrum metus dipisci ng enim sagittis augue.',
-        image: require('./assets/2Confuso.jpg')
+        image: require('./assets/2Confuso.png')
     },
     {
         key: '3',
         title: "Em dúvida",
         text: 'Lorem ipsum urna potenti etiam praesentr fringilla aliquam, vestibulum curabiturkufy imperdiet turpis nisi semper, rutrum metus dipisci ng enim sagittis augue.',
-        image: require('./assets/3EmDuvida.jpg')
+        image: require('./assets/3EmDuvida.png')
     },
     {
         key: '4',
         title: "Estou bem",
         text: 'Lorem ipsum urna potenti etiam praesentr fringilla aliquam, vestibulum curabiturkufy imperdiet turpis nisi semper, rutrum metus dipisci ng enim sagittis augue.',
-        image: require('./assets/4EstouBem.jpg')
+        image: require('./assets/4EstouBem.png')
     },
     {
         key: '5',
         title: "Feliz",
         text: 'Lorem ipsum urna potenti etiam praesentr fringilla aliquam, vestibulum curabiturkufy imperdiet turpis nisi semper, rutrum metus dipisci ng enim sagittis augue.',
-        image: require('./assets/5Feliz.jpg')
+        image: require('./assets/5Feliz.png')
     },
     {
         key: '6',
         title: "Nâo estou feliz",
         text: 'Lorem ipsum urna potenti etiam praesentr fringilla aliquam, vestibulum curabiturkufy imperdiet turpis nisi semper, rutrum metus dipisci ng enim sagittis augue.',
-        image: require('./assets/6Infeliz.jpg')
+        image: require('./assets/6Infeliz.png')
     },
     {
         key: '7',
         title: "Triste",
         text: 'Lorem ipsum urna potenti etiam praesentr fringilla aliquam, vestibulum curabiturkufy imperdiet turpis nisi semper, rutrum metus dipisci ng enim sagittis augue.',
-        image: require('./assets/7Triste.jpg')
+        image: require('./assets/7Triste.png')
     },
     {
         key: '8',
         title: "Estou com Raiva",
         text: 'Lorem ipsum urna potenti etiam praesentr fringilla aliquam, vestibulum curabiturkufy imperdiet turpis nisi semper, rutrum metus dipisci ng enim sagittis augue.',
-        image: require('./assets/8Raiva.jpg')
+        image: require('./assets/8Raiva.png')
     },
     {
         key: '9',
         title: "Surpreso",
         text: 'Lorem ipsum urna potenti etiam praesentr fringilla aliquam, vestibulum curabiturkufy imperdiet turpis nisi semper, rutrum metus dipisci ng enim sagittis augue.',
-        image: require('./assets/9Surpreso.jpg')
+        image: require('./assets/9Surpreso.png')
     },
 ]
--- src/screens/Activities/Routines/data.js
+++ src/screens/Activities/Routines/data.js
@@ -3,54 +3,54 @@ export default [
         key: '1',
         title: "Acordar",
         text: 'Lorem ipsum urna potenti etiam praesentr fringilla aliquam, vestibulum curabiturkufy imperdiet turpis nisi semper, rutrum metus dipisci ng enim sagittis augue.',
-        image: require('./assets/1Acordar.jpg')
+        image: require('./assets/1Acordar.png')
     },
     {
         key: '2',
         title: "Escovar os Dentes",
         text: 'Lorem ipsum urna potenti etiam praesentr fringilla aliquam, vestibulum curabiturkufy imperdiet turpis nisi semper, rutrum metus dipisci ng enim sagittis augue.',
-        image: require('./assets/2EscovarOsDentes.jpg')
+        image: require('./assets/2EscovarOsDentes.png')
     },
     {
         key: '3',
         title: "Tomar Banho",
         text: 'Lorem ipsum urna potenti etiam praesentr fringilla aliquam, vestibulum curabiturkufy imperdiet turpis nisi semper, rutrum metus dipisci ng enim sagittis augue.',
-        image: require('./assets/3TomarBanho.jpg')
+        image: require('./assets/3TomarBanho.png')
     },
     {
         key: '4',
         title: "Café da Manhã",
         text: 'Lorem ipsum urna potenti etiam praesentr fringilla aliquam, vestibulum curabiturkufy imperdiet turpis nisi semper, rutrum metus dipisci ng enim sagittis augue.',
-        image: require('./assets/4CafeDaManha.jpg')
+        image: require('./assets/4CafeDaManha.png')
     },
     {
         key: '5',
         title: "Usar o Banheiro",
         text: 'Lorem ipsum urna potenti etiam praesentr fringilla aliquam, vestibulum curabiturkufy imperdiet turpis nisi semper, rutrum metus dipisci ng enim sagittis augue.',
-        image: require('./assets/5UsarBanheiro.jpg')
+        image: require('./assets/5UsarBanheiro.png')
     },
     {
         key: '6',
         title: "Ir para escola",
         text: 'Lorem ipsum urna potenti etiam praesentr fringilla aliquam, vestibulum curabiturkufy imperdiet turpis nisi semper, rutrum metus dipisci ng enim sagittis augue.',
-        image: require('./assets/6IrParaEscola.jpg')
+        image: require('./assets/6IrParaEscola.png')
     },
     {
         key: '7',
         title: "Estudar",
         text: 'Lorem ipsum urna potenti etiam praesentr fringilla aliquam, vestibulum curabiturkufy imperdiet turpis nisi semper, rutrum metus dipisci ng enim sagittis augue.',
-        image: require('./assets/7Estudar.jpg')
+        image: require('./assets/7Estudar.png')
     },
     {
         key: '8',
         title: "Praticar Esportes",
         text: 'Lorem ipsum urna potenti etiam praesentr fringilla aliquam, vestibulum curabiturkufy imperdiet turpis nisi semper, rutrum metus dipisci ng enim sagittis augue.',
-        image: require('./assets/8PraticarEsportes.jpg')
+        image: require('./assets/8PraticarEsportes.png')
     },
     {
         key: '9',
         title: "Ir Dormir",
         text: 'Lorem ipsum urna potenti etiam praesentr fringilla aliquam, vestibulum curabiturkufy imperdiet turpis nisi semper, rutrum metus dipisci ng enim sagittis augue.',
-        image: require('./assets/9Dormir.jpg')
+        image: require('./assets/9Dormir.png')
     },
 ]
