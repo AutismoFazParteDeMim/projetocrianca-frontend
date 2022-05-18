31b63b761967f5a0bfbf7fab2666c10f0ecf537b
João Sereia <joao.lo.sereia@gmail.com>
2022-05-18 19:18:34 -0300

    att texto about

--- src/screens/About/index.js
+++ src/screens/About/index.js
@@ -1,11 +1,11 @@
-import React,{useState, useEffect} from "react"
-import {Linking} from "react-native"
-import {Container, GitHubButton, Image, View, TextAbout, ScrollView} from "./styles"
-import {getGitData} from "./api"
-import {Text} from "../../components"
+import React, { useState, useEffect } from "react"
+import { Linking } from "react-native"
+import { Container, GitHubButton, Image, View, TextAbout, ScrollView } from "./styles"
+import { getGitData } from "./api"
+import { Text } from "../../components"
 
 
-export default function About ({ navigation }) {
+export default function About({ navigation }) {
     const [data, setData] = useState([{
         "login": "lracha",
         "id": 92896125,
@@ -26,48 +26,43 @@ export default function About ({ navigation }) {
         "type": "User",
         "site_admin": false,
         "contributions": 3
-      },])
+    },])
 
     useEffect(() => {
         const getData = async () => {
             let data = await getGitData()
             setData(data.data)
-            
+
         }
 
         getData()
-    },[])
-   
+    }, [])
 
-    return ( 
-    <Container>
-        <TextAbout>A idealizadora do projeto foi a Rose, uma mãe dedicada e uma cidadã exemplar, que 
-            criou uma apostila para lidar com as dificuldades que a mesma tinha com a sua filha, 
-            e além disso fez cópias das apostilas para ajudar outras mães que enfrentam a mesma 
-            dificuldade. Com a ajuda do Farol eles entraram em contato com a Unisociesc que 
-            através dos projetos de extensão, os alunos que tiveram interesse no projeto que a 
-            Rose havia idealizado se inscreveram no projeto e começaram a fazer ele andar, 
-            juntamente com a mentoria do Diego Possamai os alunos colocaram a mão na massa 
-            e desde então o projeto vem crescendo cada vez mais e auxiliando várias famílias. 
-        </TextAbout>
 
-        <View>
-            <Text>Desenvolvedores: </Text>
-            <ScrollView horizontal = {true}>
-                {data.map((item, index) => (
-                    <Image key={index} source={{uri:item.avatar_url}}/>
-                    ) ) }
-                
-            </ScrollView>
-        </View>
+    return (
+        <Container>
+            <TextAbout>
+                A idealizadora do projeto Rose, uma mãe dedicada e uma cidadã exemplar, criou uma apostila para lidar com as dificuldades que ela mesma tinha em criar sua filha, e, além disso, fez cópias para ajudar outras mães que enfrentam a mesma dificuldade. Com a ajuda do Farol entraram em contato com a Unisociesc que através dos projetos de extensão, permitiu o desenvolvimento de um aplicativo que conterá muito mais do que o conteúdo da apostila.
+                Com mentoria de Diego Possamai o aplicativo começou a ser desenvolvido em 2020 pelos alunos e hoje já conta com algumas atividades, jogos educativos e a personalização do avatar.
+            </TextAbout>
 
-      
-        
+            <View>
+                <Text>Desenvolvedores: </Text>
+                <ScrollView horizontal={true}>
+                    {data.map((item, index) => (
+                        <Image key={index} source={{ uri: item.avatar_url }} />
+                    ))}
 
-        <GitHubButton title="GitHub" icon="logo-github" type="Custom" onPress ={()=> Linking.openURL("https://github.com/AutismoFazParteDeMim/ProjetoCrianca")}/>
+                </ScrollView>
+            </View>
 
-    </Container>
-    
+
+
+
+            <GitHubButton title="GitHub" icon="logo-github" type="Custom" onPress={() => Linking.openURL("https://github.com/AutismoFazParteDeMim/ProjetoCrianca")} />
+
+        </Container>
+
     )
 
 
