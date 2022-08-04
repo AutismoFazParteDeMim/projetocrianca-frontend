84e6ed0ca60b59cb65655ce765dcab5d7b94a5ab
João Sereia <joao.lo.sereia@gmail.com>
2022-08-03 21:36:34 -0300

    correção de bugs

--- App.js
+++ App.js
@@ -1,24 +1,30 @@
 import "expo-dev-client"
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useFonts, Poppins_400Regular, Poppins_500Medium, Poppins_700Bold } from '@expo-google-fonts/poppins'
 import AppLoading from 'expo-app-loading'
 import { NavigationContainer } from "@react-navigation/native"
 import { ThemeProvider } from 'styled-components/native'
 import { ThemeProvider as RNEThemeProvider } from 'react-native-elements'
 import { StatusBar } from "expo-status-bar"
-import { Provider as StoreProvider, useSelector } from 'react-redux'
+import { Provider as StoreProvider, useSelector, useDispatch } from 'react-redux'
 import { store, persistor } from './src/redux'
 import { PersistGate } from "redux-persist/integration/react"
 import Routes from "./src/navigation"
 import { LogBox } from 'react-native'
+import { reset } from './src/redux/modules/settings/actions'
 
 function AppContent() {
+    const dispatch = useDispatch()
     const { theme } = useSelector((state) => state.settings)
-    let [fontsLoaded] = useFonts({
+    let [ fontsLoaded ] = useFonts({
         "regular": Poppins_400Regular,
         "medium": Poppins_500Medium,
         "bold": Poppins_700Bold
     })
+
+    useEffect(() => {
+        dispatch(reset())
+    }, [])
 
     if (!fontsLoaded) {
         return <AppLoading />
@@ -46,5 +52,5 @@ export default function App() {
     )
 }
 
-LogBox.ignoreLogs(['Setting a timer'])
-LogBox.ignoreLogs(['AsyncStorage'])
+LogBox.ignoreLogs([ 'Setting a timer' ])
+LogBox.ignoreLogs([ 'AsyncStorage' ])
